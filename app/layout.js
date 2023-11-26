f20cfc10a19ea42e018c46116b3edb0086f90a3d
amandardz <amandardz15@gmail.com>
2023-11-26 16:30:54 -0600

    rearranging components such as nav and footer so they are reusable

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,10 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Inter } from 'next/font/google';
+import './globals.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import Navigation from './components/Navigation/navigation';
+import Footer from './components/Footer/footer';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Amanda Rodriguez',
@@ -10,12 +12,18 @@ export const metadata = {
   icons: {
     icon: '/favicon.ico',
   },
-}
+};
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" data-theme="fantasy">
-      <body className={inter.className}>{children}</body>
+    <html lang='en' data-theme='fantasy'>
+      <body className={inter.className}>
+        <main className='bg-base-content brightness-150, contrast-150 flex flex-col justify-between min-h-screen'>
+          <Navigation />
+            {children}
+          <Footer />
+        </main>
+      </body>
     </html>
-  )
+  );
 }
--- app/page.js
+++ app/page.js
@@ -1,13 +1,5 @@
-import Footer from './components/Footer/footer';
 import Hero from './components/Hero/hero';
-import Navigation from './components/Navigation/navigation';
 
 export default function Home() {
-  return (
-    <main className='bg-base-content brightness-150, contrast-150 min-h-screen flex flex-col justify-between'>
-      <Navigation />
-      <Hero />
-      <Footer />
-    </main>
-  );
+  return <Hero />;
 }
