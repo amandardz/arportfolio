112db015873045039bd38d39ab1424ec58706645
amandardz <amandardz15@gmail.com>
2023-11-26 16:29:42 -0600

    Implementing Link Component to avoid reloading the resources that will appear consistently

--- app/components/Navigation/navigation.js
+++ app/components/Navigation/navigation.js
@@ -1,11 +1,27 @@
 'use client';
+import Link from 'next/link';
 
 export default function Navigation() {
   return (
     <ul className='menu menu-horizontal bg-neutral-content flex justify-end text-xl'>
-      <li className='tooltip tooltip-bottom tooltip-primary' data-tip='home'><a href=''><i className='bi bi-house'></i></a></li>
-      <li className='tooltip tooltip-bottom tooltip-primary' data-tip='about'><a href=''><i className='bi bi-person-vcard'></i></a></li>
-      <li className='tooltip tooltip-bottom tooltip-primary' data-tip='portfolio'><a href=''><i className='bi bi-code-square'></i></a></li>
+      <li className='tooltip tooltip-bottom tooltip-primary' data-tip='home'>
+        <Link href='/'>
+          <i className='bi bi-house'></i>
+        </Link>
+      </li>
+      <li className='tooltip tooltip-bottom tooltip-primary' data-tip='about'>
+        <Link href='/about'>
+          <i className='bi bi-person-vcard'></i>
+        </Link>
+      </li>
+      <li
+        className='tooltip tooltip-bottom tooltip-primary'
+        data-tip='portfolio'
+      >
+        <Link href=''>
+          <i className='bi bi-code-square'></i>
+        </Link>
+      </li>
     </ul>
   );
 }
